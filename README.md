be10734dfb960f9726378dcb20dc3c8d3b638010
SivaReddyk608 <sivareddyk608@gmail.com>
2020-11-18 01:44:15 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-14htns
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-14htns)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+import TodoList from "./Components/TodoList";
+
+export default function App() {
+  return (
+    <div>
+      <TodoList />
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
